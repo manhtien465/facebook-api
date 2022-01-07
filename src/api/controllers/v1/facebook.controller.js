f4fd5b399ecbb4a6dfd22a35dc0e67e02923e6fa
manhtien4655 <tien.nm@csell.com.vn>
2022-01-07 16:13:59 +0700

    change to gitlab1

--- src/api/controllers/v1/facebook.controller.js
+++ src/api/controllers/v1/facebook.controller.js
@@ -19,19 +19,16 @@ exports.confirm = async (req, res, next) => {
  * receive
  */
 exports.recieve = async (req, res, next) => {
-    // const body = req.body;
+    const body = req.body;
     console.log('đã có webhook');
     console.log(req.body);
     // Checks this is an event from a page subscription
-    // if (body.object === 'page') {
-    // // Iterates over each entry - there may be multiple if batched
-    //     body.entry.forEach((entry) => {
-    //         // Gets the message. entry.messaging is an array, but
-    //         // will only ever contain one message, so we get index 0
-    //         const webhook_event = entry.messaging[0];
-    //         console.log(webhook_event);
-    //     });
-
+    if (body.object === 'page') {
+    // Iterates over each entry - there may be multiple if batched
+        body.entry.forEach((entry) => {
+            console.log(entry);
+        });
+    }
     // Returns a '200 OK' response to all requests
     res.status(200).send('EVENT_RECEIVED');
     // } else {
